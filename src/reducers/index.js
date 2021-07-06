69f67fc6925bf6653eee25e56b052b0c2a66517d
DerekChou <derekchou3704@gmail.com>
2021-07-07 00:17:50 +0800

    Add 2 reducers and a combinedReducers from Redux

---
+++ src/reducers/index.js
@@ -0,0 +1,38 @@
+import { combinedReducers } from 'redux'
+
+
+//  A static Array
+const songsReducer = () => {
+    return [
+        {
+            title: 'No Scrubs', 
+            duration: '4:05'
+        },
+        {
+            title: 'Macarena',
+            duration: '2:30'
+        },
+        {
+            title:'All Star',
+            duration: '3:15'
+        },
+        {
+            title: 'I Want It That Way',
+            duration: '1:45'
+        }
+    ]
+}
+
+const selectedSongReducer = (selectSong = null, action) => {
+    // In case there are additional options or types
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload
+    }
+
+    return selectSong
+}
+
+combinedReducers({
+    songs: songsReducer,
+    selectSong: selectedSongReducer
+})
